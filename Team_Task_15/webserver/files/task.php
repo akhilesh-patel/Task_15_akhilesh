6977f4a729e69b5549c49c9dcfb97ed87de83a61
akhilesh-patel <64592542+akhilesh-patel@users.noreply.github.com>
2021-03-14 08:45:58 +0530

    Add files via upload

---
+++ Team_Task_15/webserver/files/task.php
@@ -0,0 +1,13 @@
+<!Doctype html>
+<html>
+<body>
+<h1>
+<marquee> configuring web server using ansible and haproxy load_balancer </marquee>
+</h1>
+<pre>
+<?php
+print `/usr/sbin/ifconfig`;
+?>
+</pre>
+</body>
+</html>
